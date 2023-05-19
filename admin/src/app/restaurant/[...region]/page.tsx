56f2376c515735325bd9cc39a044c431daef4020
haewonc <haewon858@gmail.com>
2023-05-20 03:50:50 +0900

    editable cells

--- admin/src/app/restaurant/[...region]/page.tsx
+++ admin/src/app/restaurant/[...region]/page.tsx
@@ -1,63 +1,110 @@
 "use client";
 import { useEffect, useState } from 'react';
 
-export default function RestaurantPage({ params }: { params: { region: string[] } }) {
-    const data = Array(4).fill(
-        {
-            "name": "Asobu",
-            "introduction": "A best place for KAIST students",
-            "address": "14, Eoeun-ro 48beon-gil, Yuseong-gu, Daejeon-si",
-            "location": [34.21, 66.55],
-            "phone": "042-XXX-XXXX",
-            "price": 9500,
-            "business_hour": {
-                "Mon": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-                "Tue": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-                "Wed": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-                "Thu": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-                "Fri": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-                "Sat": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-                "Sun": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-            },
-            "moods": ["Cozy", "Affordable"],
-            "characteristics": ["Sushi", "Donburi"],
-            "images": ["path/to/image1"],
-            "menus": {
-                "menu A": 9500,
-                "menu B": 11000,
-            },
-            "reviews": [ {
-                "username": "minji",
-                "content": "Good"
-                    }],
-            "rating": 4.6
-        }, 
-    )
+interface EditableCellProps {
+  value: string;
+  onValueChange: (value: string) => void;
+}
 
+const EditableCell: React.FC<EditableCellProps> = ({ value, onValueChange }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedValue, setEditedValue] = useState(value);
+
+  useEffect(() => {
+    setEditedValue(value);
+  }, [value]);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+    onValueChange(editedValue);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedValue(e.target.value);
+  };
+
+  return isEditing ? (
+    <td>
+      <input
+        value={editedValue}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        autoFocus
+        style={{ width: '100%', boxSizing: 'border-box' }}
+      />
+    </td>
+  ) : (
+    <td className="border px-4 py-2" onClick={handleEdit}>{value}</td>
+  );
+}
+
+export default function RestaurantPage({ params }: { params: { region: string[] } }) {
+    const data = Array(6).fill(0).map(() => (
+      {
+        "name": "Asobu",
+        "introduction": "A best place for KAIST students",
+        "address": "14, Eoeun-ro 48beon-gil, Yuseong-gu, Daejeon-si",
+        "location": [34.21, 66.55],
+        "phone": "042-XXX-XXXX",
+        "price": 9500,
+        "business_hour": {
+          "Mon": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+          "Tue": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+          "Wed": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+          "Thu": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+          "Fri": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+          "Sat": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+          "Sun": [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        },
+        "moods": ["Cozy", "Affordable"],
+        "characteristics": ["Sushi", "Donburi"],
+        "images": ["path/to/image1"],
+        "menus": {
+          "menu A": 9500,
+          "menu B": 11000,
+        },
+        "reviews": [ {
+          "username": "minji",
+          "content": "Good"
+          }],
+        "rating": 4.6
+      }) 
+    )
+    const dataKeys = Object.keys(data[0]);
     const [tableData, setTableData] = useState<{ [key: string]: any }[]>(data);
 
-    const renderTableCell = (value: any) => {
+    const handleCellValueChange = (rowIndex: number, columnIndex: number, newValue: string) => {
+      const updatedData = [...tableData];
+      updatedData[rowIndex][dataKeys[columnIndex]] = newValue;
+      setTableData(updatedData);
+    };
+
+    const renderTableCell = (key: string, value: any) => {
         if (Array.isArray(value) || typeof value === 'object') {
-          if (typeof value === 'object' && Object.keys(value).length === 7) {
-            const weekdays = Object.keys(value);
-            return weekdays.map((day) => {
-              const hours = value[day];
-              const openRanges = [];
-              let startHour = -1;
-              for (let i = 0; i < 24; i++) {
-                if (hours[i] === 1 && startHour === -1) {
-                  startHour = i;
-                } else if (hours[i] === 0 && startHour !== -1) {
-                  openRanges.push(`${startHour}-${i-1}`);
-                  startHour = -1;
-                }
-              }
-              if (startHour !== -1) {
-                openRanges.push(`${startHour}-23`);
-              }
-              return `${day} ${openRanges.join(', ')}`;
-            }).join(', ');
-          }
+          // if (key === 'business_hour') {
+          //   const weekdays = Object.keys(value);
+          //   return weekdays.map((day) => {
+          //     const hours = value[day];
+          //     const openRanges = [];
+          //     let startHour = -1;
+          //     for (let i = 0; i < 24; i++) {
+          //       if (hours[i] === 1 && startHour === -1) {
+          //         startHour = i;
+          //       } else if (hours[i] === 0 && startHour !== -1) {
+          //         openRanges.push(`${startHour}-${i-1}`);
+          //         startHour = -1;
+          //       }
+          //     }
+          //     if (startHour !== -1) {
+          //       openRanges.push(`${startHour}-23`);
+          //     }
+          //     return `${day} ${openRanges.join(', ')}`;
+          //   }).join(', ');
+          // }
           return JSON.stringify(value);
         }
         return value;
@@ -79,14 +126,16 @@ export default function RestaurantPage({ params }: { params: { region: string[] 
                 </tr>
                 </thead>
                 <tbody>
-                {tableData.map((row) => (
-                    <tr key={row.id}>
-                    {Object.values(row).map((value, index) => (
-                        <td key={index} className="border px-4 py-2">
-                        {renderTableCell(value)}
-                        </td>
+                {tableData.map((row, rowIndex) => (
+                  <tr key={rowIndex}>
+                    {Object.values(row).map((value, columnIndex) => (
+                      <EditableCell 
+                        key={`${rowIndex}-${columnIndex}`} 
+                        value={renderTableCell(dataKeys[columnIndex], value)} 
+                        onValueChange={(newValue) => handleCellValueChange(rowIndex, columnIndex, newValue)} 
+                      />
                     ))}
-                    </tr>
+                  </tr>
                 ))}
                 </tbody>
             </table>
