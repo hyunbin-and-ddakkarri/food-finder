29721092e9a1c333d7ec523ae6bdba9101c0eb68
Jaeyong Sung <jaeyong0201@gmail.com>
2023-05-26 16:50:28 +0900

    added comments

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // this is for the next export
+    // added in next.js 13
+    // this will export the app in the /out folder
     output: "export",
 }
 
