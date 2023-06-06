e5ecb1ab56f4615be7c0a6f7f8eb48dff7e49d33
jaewonjeong0225 <jaewon0225@gmail.com>
2023-06-06 17:58:56 +0900

    ready to merge

--- public/src/app/page.tsx
+++ public/src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Users } from "./users";
+import Link from "next/link"
+
 import { gql, useQuery } from "@apollo/client";
 
 const query = gql`query getRegion($region: String!) {
@@ -10,6 +13,11 @@ const query = gql`query getRegion($region: String!) {
 }`;
 
 const Home = () => {
+
+  const regionList = [
+    'region1', 'region2', 'region3', 'region4', 'region5', 'region6'
+  ];
+
   const [showPriceFilter, setShowPriceFilter] = useState(false);
   const [showMoodFilter, setShowMoodFilter] = useState(false);
   const [showRatingFilter, setShowRatingFilter] = useState(false);
@@ -46,7 +54,7 @@ const Home = () => {
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
           </div>
-          <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required/>
+          <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
         </div>
         <button type="submit" className="p-2.5 text-blue-400 ml-0 mr-4 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
@@ -155,7 +163,20 @@ const Home = () => {
       <div className="content flex-1 overflow-y-auto">
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
           <div>
-          <img src="img/logo.png" alt="img/logo.png"/>
+            <img src="img/logo.png" alt="img/logo.png" />
+            <div className="grid grid-rows-6 grid-flow-col gap-4 max-w-md divide-y divide-gray-200 dark:divide-gray-700"> {/* 6-> list element number */}
+              {regionList.map((region, index) => (
+                <div key={index}>
+                  <div className="flex-1 items-center space-x-4 min-w-0">
+                        <button
+                          type="button"
+                          className="block w-full cursor-pointer rounded-lg p-4 text-left transition duration-500 hover:bg-neutral-100 hover:text-neutral-500 focus:bg-neutral-100 focus:text-neutral-500 focus:ring-0 dark:hover:bg-neutral-600 dark:hover:text-neutral-200 dark:focus:bg-neutral-600 dark:focus:text-neutral-200">
+                          {region}
+                        </button>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
