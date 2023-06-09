9fe2dcd5d4b3958ef60012c69c2872df30d49e58
airahr <airahr@kaist.ac.kr>
2023-06-09 18:32:12 +0900

    Generalized

--- public/src/app/datamap/page.tsx
+++ public/src/app/datamap/page.tsx
@@ -7,11 +7,11 @@ import React, {useState, useEffect, useRef, useLayoutEffect} from "react"
 
 //console.log(data().data[0].name)
 
-const loaderProp =({ src }) => {
+const loaderProp = ({ src }: { src: string }) => {
     return src;
 }
 
-const Datamap2 = () => {
+const Datamap2: React.FC = () => {
     const [h, setHeight] = useState(0)
     const [w, setWidth] = useState(0)
     const [baseX, setBaseX] = useState(0);
@@ -19,25 +19,40 @@ const Datamap2 = () => {
 
     const ref = useRef(null)
 
-    const images = ['img/Korean.jpg', 'img/Japanese.jpg', 'img/Chinese.jpg', 'img/Italian.jpg']
-    const subimages = ['img/1.jpeg', 'img/2.jpeg', 'img/3.jpeg', 'img/4.jpeg']
+    //const images = ['img/Korean.jpg', 'img/Japanese.jpg', 'img/Chinese.jpg', 'img/Italian.jpg']
+    //const subImages = ['img/1.jpeg', 'img/2.jpeg', 'img/3.jpeg', 'img/4.jpeg']
 
     useEffect(() => {
-        setHeight(ref.current.clientHeight)
-        setWidth(ref.current.clientWidth)
+        if(ref.current != null) {
+            setHeight(ref.current['clientHeight'])
+            setWidth(ref.current['clientWidth'])
+        }
     }, [])
 
 
     var lvl = 0;
     var cnt = [];
-    var coords = [];
+    var coords:[number, number, string][] = [];
 
-    var categories = [
+    var categories:{name: string, elements: string[]}[] = [
         { name: "Korean", elements: ["Bulgogi", "Ddeokbokki", "Bibimbap", "Jokbal"] },
         { name: "Japanese", elements: ["Ramen", "Sushi", "Donburi"] },
         { name: "Chinese", elements: ["Malatang"] },
         { name: "Italian", elements: ["Pizza", "Spaghetti"] }
     ]
+
+    var images: string[] = [];
+    var subImages: string[][] = [];
+
+    for(const [i, {name, elements}] of categories.entries()){
+        images.push('img/'+name+'.jpg')
+        var menus: string[] = [];
+        for(const [index, menu] of elements.entries()){
+            menus.push('img/'+name+'/'+menu+'.jpeg')
+        }
+        subImages.push(menus)
+    }
+
     var n=categories.length;
 
     for(var i=1;lvl<n;i++){
@@ -52,6 +67,7 @@ const Datamap2 = () => {
         const deg60 = Math.PI/3;
         var r, theta;
         var x, y;
+
         if(lvl==0){
             x=0;
             y=0;
@@ -97,13 +113,14 @@ const Datamap2 = () => {
     for (const [index, [x, y, name]] of coords.entries()){
         buttons.push(
             <Image
-            src={images[index]} alt={name}
+            src={images[index]}
+            alt={name}
             width={0.8*size}
             height={0.8*size}
             style={{position: "absolute", width: 1.0*size+"px", height: 1.0*size+"px", bottom: (((y-1/2)*size*1.1)+(h/2))+"px", left: (((x-1/2)*size*1.1)+(w/2))+"px"}} onClick={e => hideOtherButtons(e, index, (((y-1/2)*size*1.1)+(h/2)), (((x-1/2)*size*1.1)+(w/2)))}
             className='rounded-full'
             loader={loaderProp}/>
-            )
+        )
     }
 
     if (buttonState >= 0) {
@@ -115,8 +132,7 @@ const Datamap2 = () => {
             restaurantButtons.push(
                 <Link href='/map'>
                     <Image
-                    src={subimages[index]}
-                    alt={name}
+                    src={subImages[buttonState][index]} alt={name}
                     width={0.7*size}
                     height={0.7*size}
                     style={{position: "absolute", width: 0.7*size+"px", height: 0.7*size+"px", bottom: (((y+0.15)*size))+baseY+"px", left: (((x+0.15)*size))+baseX+"px"}} onClick={e => hideOtherButtons(e, index, (((y-1/2)*size)+(h/2)), (((x-1/2)*size)+(w/2)))}
@@ -128,7 +144,7 @@ const Datamap2 = () => {
         }
     }
 
-    return (
+    return(
         <div ref={ref} className="h-screen">
             <div>{buttons}</div>
             {restaurantButtons}
