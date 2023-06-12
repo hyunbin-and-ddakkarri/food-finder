f9587a20c38a1e26c93c07f7924b7006a4acec17
jaewonjeong0225 <jaewon0225@gmail.com>
2023-06-13 00:23:37 +0900

    markers on map

--- public/src/app/map/page.tsx
+++ public/src/app/map/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { markerdata } from '../markerdata';
 
 declare global {
     interface Window {
@@ -18,28 +20,69 @@ function Map() {
         document.head.appendChild(mapScript);
 
         const onLoadKakaoMap = () => {
-                window.kakao.maps.load(() => {
-                    const mapContainer = document.getElementById("map");
-                    const mapOption = {
-                        center: new window.kakao.maps.LatLng(36.3629, 127.3568),
-                        level: 2,
-                    };
-                    var map = new window.kakao.maps.Map(mapContainer, mapOption);
-                    var markerPosition = new window.kakao.maps.LatLng(36.363169, 127.357197);
-                    var marker = new window.kakao.maps.Marker({
-                        position: markerPosition,
-                        clickable: true
-                    });
-                    marker.setMap(map);
-
+            window.kakao.maps.load(() => {
+                const mapContainer = document.getElementById("map");
+                const mapOption = {
+                    center: new window.kakao.maps.LatLng(36.3629, 127.3584),//36.3629, 127.3568 어은동
+                    level: 2,
+                };
+                var map = new window.kakao.maps.Map(mapContainer, mapOption);
+                var markerPosition = new window.kakao.maps.LatLng(36.363169, 127.357197);
+                var marker = new window.kakao.maps.Marker({
+                    position: markerPosition,
+                    clickable: true
                 });
-          };
-          mapScript.addEventListener("load", onLoadKakaoMap);
+                marker.setMap(map);
+                // 마커를 표시할 위치와 title 객체 배열입니다 
+
+                var map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+                var positions = [
+                    {
+                        title: '버기즈',
+                        latlng: new window.kakao.maps.LatLng(36.3597, 127.3526)
+                    },
+                    {
+                        title: '아소부',
+                        latlng: new window.kakao.maps.LatLng(36.3628, 127.3578)
+                    },
+                    {
+                        title: '모티프',
+                        latlng: new window.kakao.maps.LatLng(36.3628, 127.3587)
+                    },
+                ];
+
+                // 마커 이미지의 이미지 주소입니다
+                var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+                for (var i = 0; i < positions.length; i++) {
+
+                    // 마커 이미지의 이미지 크기 입니다
+                    var imageSize = new window.kakao.maps.Size(24, 35);
+
+                    // 마커 이미지를 생성합니다    
+                    var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
+
+                    // 마커를 생성합니다
+                    var marker = new window.kakao.maps.Marker({
+                        map: map, // 마커를 표시할 지도
+                        position: positions[i].latlng, // 마커를 표시할 위치
+                        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                        image: markerImage // 마커 이미지 
+                    });
+                }
+            });
+
+        };
+        mapScript.addEventListener("load", onLoadKakaoMap);
+
     }, []);
 
     return (
         <div className="Map h-screen relative">
-            <div id="map" style={{ position: "absolute", width: "100%", height: "105vh", marginTop: "-75px", zIndex: "-1"}}></div>
+            <div id="map" style={{ position: "absolute", width: "100%", height: "105vh", marginTop: "-75px", zIndex: "-1" }}>
+
+            </div>
         </div>
     );
 };
--- public/src/app/work/page.tsx
+++ public/src/app/work/page.tsx
@@ -1,41 +1,114 @@
-"use client"
-import { DropDownTreeComponent } from '@syncfusion/ej2-react-dropdowns';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+"use client";
 
-function App() {
-  // definining the dataSource
-  let data: { [key: string]: Object }[] = [
-        { id: 1, name: 'Discover Music', hasChild: true, expanded: true },
-        { id: 2, pid: 1, name: 'Hot Singles' },
-        { id: 3, pid: 1, name: 'Rising Artists' },
-        { id: 4, pid: 1, name: 'Live Music' },
-        { id: 6, pid: 1, name: 'Best of 2017 So Far' },
-        { id: 7, name: 'Sales and Events', hasChild: true },
-        { id: 8, pid: 7, name: '100 Albums - $5 Each' },
-        { id: 9, pid: 7, name: 'Hip-Hop and R&B Sale' },
-        { id: 10, pid: 7, name: 'CD Deals' },
-        { id: 11, name: 'Categories', hasChild: true },
-        { id: 12, pid: 11, name: 'Songs' },
-        { id: 13, pid: 11, name: 'Bestselling Albums' },
-        { id: 14, pid: 11, name: 'New Releases' },
-        { id: 15, pid: 11, name: 'Bestselling Songs' },
-        { id: 16, name: 'MP3 Albums', hasChild: true },
-        { id: 17, pid: 16, name: 'Rock' },
-        { id: 18, pid: 16, name: 'Gospel' },
-        { id: 19, pid: 16, name: 'Latin Music' },
-        { id: 20, pid: 16, name: 'Jazz' },
-        { id: 21, name: 'More in Music', hasChild: true },
-        { id: 22, pid: 21, name: 'Music Trade-In' },
-        { id: 23, pid: 21, name: 'Redeem a Gift Card' },
-        { id: 24, pid: 21, name: 'Band T-Shirts' },
-    ];
-    let fields: Object = { dataSource: data, value: 'id', text: 'name', parentValue:"pid", hasChildren: 'hasChild' };
-    let treeSettings: Object = { autoCheck: true }
-    return (
-        // specifies the tag for render the DropDownTree component
-      <DropDownTreeComponent id="dropdowntree" fields={fields} showCheckBox={true} treeSettings={treeSettings}/>
-    );
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { markerdata } from '../markerdata';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
 }
-export default App;
-//ReactDOM.render(<App />, document.getElementById('sample'));
+
+function Map() {
+  useEffect(() => {
+    const mapScript = document.createElement('script');
+
+    mapScript.async = true;
+    mapScript.src = `http://dapi.window.kakao.com/v2/maps/sdk.js?appkey=4db502f3df145cbe27c3dbc6376ef4e8&autoload=false`;
+
+    document.head.appendChild(mapScript);
+   /*
+    const onLoadKakaoMap = () => {
+      window.window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById("map");
+        const mapOption = {
+          center: new window.window.kakao.maps.LatLng(33.450701, 126.570667),//36.3629, 127.3568 어은동
+          level: 2,
+        };
+        var map = new window.window.kakao.maps.Map(mapContainer, mapOption);
+        var markerPosition = new window.window.kakao.maps.LatLng(36.363169, 127.357197);
+        var marker = new window.window.kakao.maps.Marker({
+          position: markerPosition,
+          clickable: true
+        });
+        marker.setMap(map);
+        // 마커를 표시할 위치와 title 객체 배열입니다 
+
+
+        var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+        var map = new window.window.kakao.maps.Map(mapContainer, mapOption);
+
+          mapCenter = new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심 좌표
+          mapOption = {
+            center: mapCenter, // 지도의 중심 좌표
+            level: 4 // 지도의 확대 레벨
+          };
+
+        // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+        var map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+        var myPosition = new window.kakao.maps.LatLng(33.450529632322265, 126.57038644763402);
+
+        // 지도에 올릴 마커를 생성합니다.
+        var mMarker = new window.kakao.maps.Marker({
+          position: myPosition, // 지도의 중심좌표에 올립니다.
+          map: map // 생성하면서 지도에 올립니다.
+        });
+
+        // 지도에 올릴 장소명 인포윈도우 입니다.
+        var mLabel = new window.kakao.maps.InfoWindow({
+          position: myPosition, // 지도의 중심좌표에 올립니다.
+          content: '내가 일하는 곳' // 인포윈도우 내부에 들어갈 컨텐츠 입니다.
+        });
+        mLabel.open(map, mMarker); // 지도에 올리면서, 두번째 인자로 들어간 마커 위에 올라가도록 설정합니다.
+
+        var rvContainer = document.getElementById('roadview'); // 로드뷰를 표시할 div
+
+        var rv = new window.kakao.maps.Roadview(rvContainer); // 로드뷰 객체 생성
+        var rc = new window.kakao.maps.RoadviewClient(); // 좌표를 통한 로드뷰의 panoid를 추출하기 위한 로드뷰 help객체 생성 
+        var rvPosition = new window.kakao.maps.LatLng(33.450335213582655, 126.57022069762772);
+
+        rc.getNearestPanoId(rvPosition, 50, function (panoid) {
+          rv.setPanoId(panoid, rvPosition);//좌표에 근접한 panoId를 통해 로드뷰를 실행합니다.
+        });
+
+        // 로드뷰 초기화 이벤트
+        window.kakao.maps.event.addListener(rv, 'init', function () {
+
+          // 로드뷰에 올릴 마커를 생성합니다.
+          var rMarker = new window.kakao.maps.Marker({
+            position: myPosition,
+            map: rv //map 대신 rv(로드뷰 객체)로 설정하면 로드뷰에 올라갑니다.
+          });
+          rMarker.setAltitude(6); //마커의 높이를 설정합니다. (단위는 m입니다.)
+          rMarker.setRange(100); //마커가 보일 수 있는 범위를 설정합니다. (단위는 m입니다.)
+
+          // 로드뷰에 올릴 장소명 인포윈도우를 생성합니다.
+          var rLabel = new window.kakao.maps.InfoWindow({
+            content: '내가 일하는 곳'
+          });
+          rLabel.setRange(100); //마커가 보일 수 있는 범위를 설정합니다. (단위는 m입니다.)
+          rLabel.open(rv, rMarker); // open시 마커를 넣어주면, 마커의 altitude와 position값을 모두 따라 갑니다.
+
+          // 로드뷰 마커가 중앙에 오도록 로드뷰의 viewpoint 조정합니다.
+          var projection = rv.getProjection(); // viewpoint(화면좌표)값을 추출할 수 있는 projection 객체를 가져옵니다.
+
+          // 마커의 position과 altitude값을 통해 viewpoint값(화면좌표)를 추출합니다.
+          var viewpoint = projection.viewpointFromCoords(rMarker.getPosition(), rMarker.getAltitude());
+          rv.setViewpoint(viewpoint); //로드뷰에 뷰포인트를 설정합니다.
+        });
+      }, []);
+
+      return (
+        <div className="Map h-screen relative">
+          <div id="map" style={{ position: "absolute", width: "100%", height: "105vh", marginTop: "-75px", zIndex: "-1" }}>
+
+          </div>
+        </div>
+      );
+    };*/
+  }
+  );
+};
+    export default Map;
