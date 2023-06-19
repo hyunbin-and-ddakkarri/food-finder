2bd687da528a6891556f383afc589c293872d8fb
hyunbin <jshyunbin@gmail.com>
2023-06-20 05:06:41 +0900

    adding test

--- public/__test__/page.test.tsx
+++ public/__test__/page.test.tsx
@@ -1,11 +1,12 @@
 import Home from "@/app/page";
 import '@testing-library/jest-dom'
 import { render, screen } from "@testing-library/react";
+import React from "react";
 
 describe('Main Page', () => {
     it('should render the main page', () => {
         render(<Home />);
 
-        expect(screen.getByText(/Get started by editing/i)).toBeInTheDocument();
+        expect(screen.getByText("Search")).toBeInTheDocument();
     });
 });
