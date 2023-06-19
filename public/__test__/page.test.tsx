4ebb6fc1b1bb8688c529409950d454e81c0fd19a
hyunbin <jshyunbin@gmail.com>
2023-06-20 05:11:26 +0900

    fixed test

--- public/__test__/page.test.tsx
+++ public/__test__/page.test.tsx
@@ -2,10 +2,11 @@ import Home from "@/app/page";
 import '@testing-library/jest-dom'
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import AppLayout from "../src/app/layout";
 
 describe('Main Page', () => {
     it('should render the main page', () => {
-        render(<Home />);
+        render(<AppLayout><Home></Home></AppLayout>);
 
         expect(screen.getByText("Search")).toBeInTheDocument();
     });
